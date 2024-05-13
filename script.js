73e31e3b356ec3861d1bdec9ce3a44c85fd5c7ed
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2024-05-13 19:47:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu_icon=document.getElementById("menu-icon");
+let menu=document.querySelector(".nav-list");
+function showmenu(){
+    menu.style.display="flex";
+}
+function hidemenu(){
+    menu.style.display="none";
+}
